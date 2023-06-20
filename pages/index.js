8abe28a31a7188175fc9c3eeb4860e4a2633c4a5
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-20 18:22:00 -0400

    homepage correctly renders

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/homepage.module.css';
-import Welcome from "../components/Welcome";
-// import MobileWelcome from "../../components/MobileWelcome";
-
 // css
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Welcome from '../components/Welcome';
+import MobileWelcome from '../components/MobileWelcome';
+
 const IndexPage = () => {
-    const [width, setWidth] = useState(0);
-  
-    useEffect(() => {
-      const handleResize = () => {
-        setWidth(window.innerWidth);
-      };
-  
-      // Add event listener to update width on window resize
-      window.addEventListener('resize', handleResize);
-  
-      // Initial width
-      setWidth(window.innerWidth);
-  
-      // Clean up event listener on component unmount
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []);
-  
-    if (width > 540) {
-      return (
-        <>
-          <main className="home-page">
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+    setShouldRender(true);
+  }, []);
+
+  return (
+    <>
+      {shouldRender && (
+        <main className="home-page">
+          {typeof window !== 'undefined' && window.innerWidth > 540 ? (
             <Welcome />
-          </main>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <main className="home-page">
+          ) : (
             <MobileWelcome />
-          </main>
-        </>
-      );
-    }
-  };
-  
-  export default IndexPage;
+          )}
+        </main>
+      )}
+    </>
+  );
+};
+
+export default IndexPage;
