222f3734838e2c3ea569d429cb6c12f038023027
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-20 20:29:20 -0400

    added Socials page

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ import '../styles/footer.css';
 import '../styles/aboutme.css';
 import '../styles/portfolio.css';
 import '../styles/services.css';
+import '../styles/socials.css';
 
 // components
 import Layout from './Layout';
