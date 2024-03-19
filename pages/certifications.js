3cc0ecb5865ce203536d6dfab313d850f1c8db6e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-19 14:36:21 -0400

    added certifications page

---
+++ pages/certifications.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+
+function Certifications() {
+
+    return(
+        <>
+        <Head>
+        <title> Certifications | Kaileb Hammontree</title>
+        <meta name="description" content="Learn about what qualifies Kaileb Hammontree to tackle your next project." />
+        <meta name="keywords" content="certifications, education, Kaileb Hammontree, University Of Central Florida, Web Developer, react, front-end, full-stack, back-end" />
+        <link rel="canonical" href="https://www.kailebhammontree.com/certifications" />
+      </Head>
+
+      <main>
+        <p>This is test text</p>
+       </main>
+        </>
+    )
+};
+
+export default Certifications;
