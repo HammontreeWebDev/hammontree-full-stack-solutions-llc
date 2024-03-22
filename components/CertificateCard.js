b0352377c61d6ff07a7c6148231834e9d8cada10
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-22 14:34:20 -0400

    added fade in for certs

--- components/CertificateCard.js
+++ components/CertificateCard.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { ZoomInRightDiv } from "../utils/Animations";
 import { Icon, enableCache } from "@iconify/react";
 // allow iconify icons to be stored locally for quicker rendering
 enableCache("local");
 
 function CertificateCard(props) {
-    return (
-        <ZoomInRightDiv className="custom-figure">
-            <div className="project-row">
-                <img src={props.src} className="custom-figure-img" alt={props.alt} />
-            </div>
 
-            <div className="project-row content-container">
+    const zoomInRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootmargin: "0px",
+            threshold: 0.5
+        };
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                } else {
+                    setIsVisible(false);
+                }
+            });
+        }, options);
+
+        if (zoomInRef.current) {
+            observer.observe(zoomInRef.current);
+        }
+
+        return () => {
+            if (zoomInRef.current) {
+                observer.unobserve(zoomInRef.current);
+            }
+        };
+    }, []);
+
+    return (
+        <div className="custom-figure">
+
+            <ZoomInRightDiv className={`project-row ${isVisible ? "visible" : ""}`} ref={zoomInRef}>
+                <img src={props.src} className="custom-figure-img" alt={props.alt} />
+            </ZoomInRightDiv>
+
+            <ZoomInRightDiv className={`project-row content-container ${isVisible ? "visible" : ""}`} ref={zoomInRef}>
                 <h1 className="project-title">{props.appTitle}</h1>
 
                 <p> <small className="certificate-disclaimer">Click the badge below to verify the authenticity of this certificate.</small></p>
@@ -22,8 +54,9 @@ function CertificateCard(props) {
                     </a>
 
                 </div>
-            </div>
-        </ZoomInRightDiv>
+            </ZoomInRightDiv>
+
+        </div>
     )
 }
 
