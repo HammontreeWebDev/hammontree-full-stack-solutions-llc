95ddbe3e0e6fd97b812c3472239aceafb2a9d46f
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-22 14:09:00 -0400

    added fadeIn animation only when element crosses the view threshold

--- components/Project.js
+++ components/Project.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { ZoomInRightDiv } from "../utils/Animations";
+import React, { useRef, useEffect, useState } from "react";
+import { FadeInDiv } from "../utils/Animations";
 import { Icon, enableCache } from "@iconify/react";
 // allow iconify icons to be stored locally for quicker rendering
 enableCache("local");
 
 function Project(props) {
+
+    const fadeInRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootmargin: "Opx",
+            threshold: 0.2
+        };
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                } else {
+                    setIsVisible(false);
+                }
+            });
+        }, options);
+
+        if (fadeInRef.current) {
+            observer.observe(fadeInRef.current);
+        }
+
+        return () => {
+            if (fadeInRef.current) {
+                observer.unobserve(fadeInRef.current);
+            }
+        };
+
+    }, []);
+
     return (
         <div className="custom-figure"
 
@@ -13,18 +45,28 @@ function Project(props) {
             }}
 
         >
-            <ZoomInRightDiv className="project-row">
-                <img src={props.src} className="custom-figure-img" alt={props.alt} />
-            </ZoomInRightDiv>
 
-            <ZoomInRightDiv className="project-row content-container">
-                <h1 className="project-title">{props.appTitle}</h1>
-                <p className="project-content"> <strong>Built With: </strong><span className="alt-color">{props.content}</span></p>
-                <div className="project-icon-container">
-                    <a className="project-links" href={props.repository} target="_blank" rel="noopener noreferrer" onClick={props.click}><Icon icon="uim:github-alt" /></a>
-                    <a className="project-links" href={props.deployment} target="_blank" rel="noopener noreferrer"><Icon icon="eos-icons:application-instance" /></a>
-                </div>
-            </ZoomInRightDiv>
+            <div ref={fadeInRef}>
+                {isVisible && (
+                    <FadeInDiv className="project-row">
+                        <img src={props.src} className="custom-figure-img" alt={props.alt} />
+                    </FadeInDiv>
+                )}
+            </div>
+
+            <div ref={fadeInRef}>
+                {isVisible && (
+                    <FadeInDiv className="project-row content-container">
+                        <h1 className="project-title">{props.appTitle}</h1>
+                        <p className="project-content"> <strong>Built With: </strong><span className="alt-color">{props.content}</span></p>
+                        <div className="project-icon-container">
+                            <a className="project-links" href={props.repository} target="_blank" rel="noopener noreferrer" onClick={props.click}><Icon icon="uim:github-alt" /></a>
+                            <a className="project-links" href={props.deployment} target="_blank" rel="noopener noreferrer"><Icon icon="eos-icons:application-instance" /></a>
+                        </div>
+                    </FadeInDiv>
+                )}
+            </div>
+
         </div>
     )
 }
