19996c2f813ad35b48ff8ed1bd9ee64e3ce1d732
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-22 12:01:43 -0400

    changed behavior of animation for project components

--- components/Project.js
+++ components/Project.js
@@ -6,26 +6,26 @@ enableCache("local");
 
 function Project(props) {
     return (
-        <ZoomInRightDiv className="custom-figure"
+        <div className="custom-figure"
 
             style={{
                 backgroundImage: `url(${props.photo_icon})`
             }}
 
         >
-            <div className="project-row">
+            <ZoomInRightDiv className="project-row">
                 <img src={props.src} className="custom-figure-img" alt={props.alt} />
-            </div>
+            </ZoomInRightDiv>
 
-            <div className="project-row content-container">
+            <ZoomInRightDiv className="project-row content-container">
                 <h1 className="project-title">{props.appTitle}</h1>
                 <p className="project-content"> <strong>Built With: </strong><span className="alt-color">{props.content}</span></p>
                 <div className="project-icon-container">
                     <a className="project-links" href={props.repository} target="_blank" rel="noopener noreferrer" onClick={props.click}><Icon icon="uim:github-alt" /></a>
                     <a className="project-links" href={props.deployment} target="_blank" rel="noopener noreferrer"><Icon icon="eos-icons:application-instance" /></a>
                 </div>
-            </div>
-        </ZoomInRightDiv>
+            </ZoomInRightDiv>
+        </div>
     )
 }
 
