0a03d8b50211462c32704bba752981e862f1d1c5
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-22 14:50:49 -0400

    added cert bg photos

--- pages/certifications.js
+++ pages/certifications.js
@@ -16,16 +16,27 @@ function Certifications() {
             <main className="portfolio-body">
 
                 {/* UCF Coding Boot Camp*/}
-                <CertificateCard src={"/img/coding-cert.webp"} verification={'https://badgr.com/public/assertions/ewkha2NyQ8-IAo-788fTUA'} alt={'UCF Full-Stack Coding Boot Camp Certificate'} appTitle={'UCF Coding Boot Camp'} />
+                <CertificateCard src={"/img/coding-cert.webp"} verification={'https://badgr.com/public/assertions/ewkha2NyQ8-IAo-788fTUA'} alt={'UCF Full-Stack Coding Boot Camp Certificate'} appTitle={'UCF Coding Boot Camp'} photo_icon={'img/bootcamp-bg.webp'}/>
+
+                {/* bootcamp-bg.webp Photo by Danny Meneses: https://www.pexels.com/photo/photo-of-turned-on-laptop-computer-943096/ */}
 
                 {/* UCF Cyber Defense Professional Certificate*/}
-                <CertificateCard src={"/img/cyber-cert.webp"} verification={'https://badgr.com/public/assertions/Rxyg7wKwTgWLC07_shNy6A'} alt={'UCF Cyber Defense Professional Certificate'} appTitle={'UCF Cyber Defense Professional Certificate'} />
+                <CertificateCard src={"/img/cyber-cert.webp"} verification={'https://badgr.com/public/assertions/Rxyg7wKwTgWLC07_shNy6A'} alt={'UCF Cyber Defense Professional Certificate'} appTitle={'UCF Cyber Defense Professional Certificate'} photo_icon={'img/cyber-bg.webp'}/>
+
+                {/* cyber-bg.webp Photo by Stefan Coders: https://www.pexels.com/photo/woman-coffee-apple-iphone-5243611/ */}
+
 
                 {/* Hacker Rank CSS Skill Verification*/}
-                <CertificateCard src={"/img/css-cert.webp"} verification={'https://www.hackerrank.com/certificates/c7a702eeeac3'} alt={'Hacker Rank CSS Skill Verification'} appTitle={'CSS Skill Certification'} />
+                <CertificateCard src={"/img/css-cert.webp"} verification={'https://www.hackerrank.com/certificates/c7a702eeeac3'} alt={'Hacker Rank CSS Skill Verification'} appTitle={'CSS Skill Certification'} photo_icon={'img/css-bg.webp'}/>
+
+                {/* css-bg.webp Photo by Karol D: https://www.pexels.com/photo/seat-sketch-paper-on-table-1111692/*/}
+
 
                 {/* Hacker Rank Front End React Role Verification*/}
-                <CertificateCard src={"/img/front-end-cert.webp"} verification={'https://www.hackerrank.com/certificates/4d0e4bd62b7c'} alt={'Hacker Rank Front End Developer for React Role Verification'} appTitle={'Frontend Developer (React) Certification'} />
+                <CertificateCard src={"/img/front-end-cert.webp"} verification={'https://www.hackerrank.com/certificates/4d0e4bd62b7c'} alt={'Hacker Rank Front End Developer for React Role Verification'} appTitle={'Frontend Developer (React) Certification'} photo_icon={'img/react-bg.webp'}/>
+
+                {/* react-bg.webp Photo by cottonbro studio: https://www.pexels.com/photo/workstation-while-working-on-the-webpage-6804581/*/}
+
 
             </main>
         </>
