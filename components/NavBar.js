4dae9d9fc43c0d1f722f2d9d9165b1033a23421e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-23 00:58:41 -0400

    added collapsible navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,17 +3,25 @@ import { useRouter } from "next/router";
 import { BounceInDownHeader } from "../utils/Animations";
 import Dropdown from "react-bootstrap/Dropdown";
 import Image from "next/image";
+import { Icon } from "@iconify/react";
 
 export function NavBar(props) {
   const router = useRouter();
   const [highlightedButton, setHighlightedButton] = useState(null);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const highlightNav = (e) => {
     setHighlightedButton(e.target.innerText);
+    setIsMobileMenuOpen(false);
   };
 
   const handleClick = (navigateTo) => {
     router.push(navigateTo);
+    setIsMobileMenuOpen(false);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
@@ -25,11 +33,11 @@ export function NavBar(props) {
       <img
         className="nav-img"
         src="/img/trans-web-header.webp"
-        alt="Avatar of Kaileb with his name and text that says Full-Stack Web Development" 
+        alt="Avatar of Kaileb with his name and text that says Full-Stack Web Development"
       />
 
       <nav className="container-fluid custom-nav">
-       {/* Home Button */}
+        {/* Home Button */}
         <button
           className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Home" ? "highlighted" : ""
             }`}
@@ -49,19 +57,19 @@ export function NavBar(props) {
           <Dropdown.Menu id="custom-menu">
 
             <Dropdown.Item>
-            <button
-          className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Biography" ? "highlighted" : ""
-            }`}
-          onClick={(e) => {
-            highlightNav(e);
-            handleClick("/about_me");
-          }}
-        >
-          Biography
-        </button>
+              <button
+                className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Biography" ? "highlighted" : ""
+                  }`}
+                onClick={(e) => {
+                  highlightNav(e);
+                  handleClick("/about_me");
+                }}
+              >
+                Biography
+              </button>
             </Dropdown.Item>
             <Dropdown.Item>
-            <button
+              <button
                 className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Socials" ? "highlighted" : ""
                   }`}
                 onClick={(e) => {
@@ -82,16 +90,16 @@ export function NavBar(props) {
           <Dropdown.Menu id="custom-menu">
 
             <Dropdown.Item>
-            <button
-          className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Services" ? "highlighted" : ""
-            }`}
-          onClick={(e) => {
-            highlightNav(e);
-            handleClick("/services");
-          }}
-        >
-          Services
-        </button>
+              <button
+                className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Services" ? "highlighted" : ""
+                  }`}
+                onClick={(e) => {
+                  highlightNav(e);
+                  handleClick("/services");
+                }}
+              >
+                Services
+              </button>
             </Dropdown.Item>
             <Dropdown.Item>
               <button
@@ -129,16 +137,16 @@ export function NavBar(props) {
             </Dropdown.Item>
 
             <Dropdown.Item>
-            <button
-          className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Portfolio" ? "highlighted" : ""
-            }`}
-          onClick={(e) => {
-            highlightNav(e);
-            handleClick("/portfolio");
-          }}
-        >
-          Portfolio
-        </button>
+              <button
+                className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Portfolio" ? "highlighted" : ""
+                  }`}
+                onClick={(e) => {
+                  highlightNav(e);
+                  handleClick("/portfolio");
+                }}
+              >
+                Portfolio
+              </button>
             </Dropdown.Item>
 
             <Dropdown.Item>
@@ -153,9 +161,113 @@ export function NavBar(props) {
                 Certifications
               </button>
             </Dropdown.Item>
-            
+
           </Dropdown.Menu>
         </Dropdown>
+      </nav>
+
+      <nav className="custom-mobile-nav">
+        {/* Mobile Menu Button*/}
+        <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
+          <Icon icon="line-md:menu-unfold-right" />
+        </button>
+
+        {/* Mobile Menu */}
+
+        <div className={`mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+          {/* Close out menu button */}
+          <button className="close-menu-btn" onClick={toggleMobileMenu}>
+          <Icon icon="line-md:close-small" />
+            </button>
+          {/* Home Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Home" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/");
+            }}
+          >
+            Home
+          </button>
+
+          {/* Bio Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Biography" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/about_me");
+            }}
+          >
+            Biography
+          </button>
+
+          {/* Socials Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Socials" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/socials");
+            }}
+          >
+            Socials
+          </button>
+
+          {/* Services Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Services" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/services");
+            }}
+          >
+            Services
+          </button>
+
+          {/* Contact Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Contact" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/contact");
+            }}
+          >
+            Contact
+          </button>
+
+          {/* Download Resume Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Download Resume" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/resume");
+            }}
+          >
+            Download Resume
+          </button>
+
+          {/* Portfolio Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Portfolio" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/portfolio");
+            }}
+          >
+            Portfolio
+          </button>
+
+          {/* Certifications Button */}
+          <button
+            className={`nav-font hvr-wobble-horizontal ${highlightedButton === "Certifications" ? "highlighted" : ""}`}
+            onClick={(e) => {
+              highlightNav(e);
+              handleClick("/certifications");
+            }}
+          >
+            Certifications
+          </button>
+
+        </div>
       </nav>
     </BounceInDownHeader>
   );
